49c704734276ebc6a7a660ee91e33bde1f1f74cc
ChikumbutsoGerson <chikugerson127@gmail.com>
2024-01-20 18:41:23 +0200

    NavBar Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div>
-     Home Page
-    </div>
-  );
+  return <div>Home Page</div>;
 }
--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,11 +1,58 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
+import Container from "../Container";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import SearchInput from "../SearchInput";
 
 const NavBar = () => {
+  const router = useRouter();
+
+  const { userId } = useAuth();
+
   return (
-    <div className="h-screen flex justify-end">
-      <UserButton afterSignOutUrl="/" />
+    <div className="sticky top-0 border border-b-primary/10 bg-secondary">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div
+            className="flex items-center gap-1 cursor-pointer"
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            <Image src="/logo.svg" alt="logo" width="30" height="30" />
+            <div className="font-bold text-xl">Stay Savvy</div>
+          </div>
+          <SearchInput />
+          <div className="flex gap-3 items-center">
+            <div>theme</div>
+            <UserButton afterSignOutUrl="/" />
+            {!userId && (
+              <>
+                <Button
+                  variant={"outline"}
+                  size={"sm"}
+                  onClick={() => {
+                    router.push("/sign-in");
+                  }}
+                >
+                  Sign in
+                </Button>
+                <Button
+                  size={"sm"}
+                  onClick={() => {
+                    router.push("/sign-up");
+                  }}
+                >
+                  Sign up
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
